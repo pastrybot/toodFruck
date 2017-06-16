48291277a899b8a523a5b142d86ab3557ff72912
Katie Dahlin <pastry.katie@gmail.com>
2017-06-16 12:05:10 -0600

    'Deleted map component and container'

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, browserHistory, IndexRoute} from 'react-router';
-import {HomeContainer} from './containers';
-
+import {Router, Route, browserHistory, IndexRoute} from 'react-router'
 require('bootstrap/dist/css/bootstrap.css');
 
 import App from './App';
+import {HomeContainer}
 //this is where we define the path.  You can name the path anything you want.
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <Route path="/home" component={HomeContainer} />
+
+
+
       <IndexRoute component={HomeContainer} />
     </Route>
   </Router>,
