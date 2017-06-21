465be631c867972eb503cb91203c2ddbb9714faf
Katie Dahlin <pastry.katie@gmail.com>
2017-06-20 20:38:44 -0600

    finally got map to render, and found it's comtainer

--- client/src/components/Home/Map.js
+++ client/src/components/Home/Map.js
@@ -2,23 +2,35 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 
+
 const Map = ({ text  }) => <div>{text}</div>;
 
 class TruckMap extends Component {
   static defaultProps = {
     center: {lat: 45.6770, lng: -111.0429},
-    zoom: 11
+    zoom: 11,
+    
+
   };
 
 
+
+
 render(){
+  const mapStyle = {
+      width: 500,
+      height: 300,
+      border: '1px solid black'
+    };
+
   return (
-    <div>
+    <div ref="map" style={mapStyle}>
+
     <GoogleMapReact
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
           bootstrapURLKeys={{key:'AIzaSyCsXWyNVyTn1_WFbABZWA0NHk-qsNwx0gg'}}
-          style={{height: '80%', width: 400}}
+
         >
           <Map
             lat={45.6770}
