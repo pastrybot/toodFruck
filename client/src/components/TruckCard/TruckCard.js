ffb09e6d50efb4ad34732ca04591a5b7cdeb2b0d
Katie Dahlin <pastry.katie@gmail.com>
2017-06-29 19:00:21 -0600

    social media icons showing

--- client/src/components/TruckCard/TruckCard.js
+++ client/src/components/TruckCard/TruckCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {truckCard, name, image, location,
- hours, description, info, wrapper, facebook } from './cardStyle.css';
+ hours, description, info, wrapper, facebook, instagram, icons, twitter } from './cardStyle.css';
 import {Link} from 'react-router';
 
   const TruckCard = (props) => {
@@ -10,12 +10,20 @@ import {Link} from 'react-router';
           <h2 className={name}>{ props.truck.name }</h2>
           <img className={image} alt="" src={ props.truck.img }/>
             <div className={info}>
-              <div className={hours}>{ props.truck.hours }</div>
+              <div className={hours}>Hours: { props.truck.hours }</div>
               <div className={description}>{ props.truck.description }</div>
-              <a target="_blank" href={"http://www."+ props.truck.social.facebook}>
-                <div className={facebook}></div>
-              </a>
-              
+              <div className={icons}>
+                <a target="_blank" href={"https://www.facebook.com/"+ props.truck.social.facebook}>
+                  <div className={facebook}></div>
+                </a>
+                <a target="_blank" href={"http://www.instagram.com/"+ props.truck.social.instagram}>
+                  <div className={instagram}></div>
+                </a>
+                <a target="_blank" href={"https://twitter.com/"+ props.truck.social.twitter}>
+                  <div className={twitter}></div>
+                </a>
+              </div>
+
             </div>
         </div>
       </div>
