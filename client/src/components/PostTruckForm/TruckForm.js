53416632829c2ecce2e5094a41c0d7a867f5f4c9
asternaut <snooks.sheild@gmail.com>
2017-07-13 07:34:59 -0600

    form styles

--- client/src/components/PostTruckForm/TruckForm.js
+++ client/src/components/PostTruckForm/TruckForm.js
@@ -1,62 +1,63 @@
 import React from 'react';
-import {formContainer} from "./FormStyle.css"
+import {postFormContainer, postButton, latLng} from "./FormStyle.css"
 const PostTruckForm = (props) => {
   return (
-    <div className={formContainer} >
+    <div className={postFormContainer} >
+      <h3> Add your truck: </h3>
       <form className="" onSubmit={(event) => props.handleSubmit(event)}>
-      <h1 className="">Enter Truck Name</h1>
         <div className="">
           <label className="">Name:</label>
-          <input type="text" placeholder="Name"
+          <input type="text"
             onChange={(event) => props.onChange("name", event.target.value)}/>
         </div>
-        <div className="">
-          <label className="">Image:</label>
-          <input type="url" placeholder="Image URL"
-            onChange={(event) => props.onChange("img", event.target.value)}/>
-        </div>
-        <div className="">
-          <label className="">Latitude:</label>
-          <input type="text" placeholder="Latitude"
-            onChange={(event) => props.onChange("lat", event.target.value)}/>
+        <div className={latLng}>
+          <div>
+            <label className="">Image:</label>
+            <input type="url" placeholder="Image URL"
+              onChange={(event) => props.onChange("img", event.target.value)}/>
+          </div>
+          <div>
+            <label className="">Latitude:</label>
+            <input type="text" placeholder="example: 45.5"
+              onChange={(event) => props.onChange("lat", event.target.value)}/>
+          </div>
         </div>
         <div className="">
           <label className="">Longitude:</label>
-          <input type="text" placeholder="Longitude"
+          <input type="text" placeholder="example: -111.1"
             onChange={(event)=> props.onChange("lng", event.target.value)}/>
         </div>
         <div className="">
           <label className="">Hours:</label>
-          <input type="text" placeholder="Open Hours"
+          <input type="text"
             onChange={(event) => props.onChange("hours", event.target.value)}/>
         </div>
         <div className="">
           <label className="">Description:</label>
-          <input type="text" placeholder="Description"
+          <input type="text"
             onChange={(event) => props.onChange("description", event.target.value)}/>
         </div>
         <div className="">
           <label className="">Website:</label>
-          <input type="text" placeholder="Company site URL"
+          <input type="text"
             onChange={(event) => props.onChange("website", event.target.value)}/>
         </div>
         <div className="">
           <label className="">Facebook:</label>
-          <input type="text" placeholder="Facebook"
+          <input type="text" placeholder="www.facebook.com/"
             onChange={(event) => props.onChange("facebook", event.target.value)}/>
         </div>
         <div className="">
           <label className="">Instagram:</label>
-          <input type="text" placeholder="Instagram handle"
+          <input type="text" placeholder="@"
             onChange={(event) => props.onChange("instagram", event.target.value)}/>
         </div>
         <div className="">
           <label className="">Twitter:</label>
-          <input type="text" placeholder="Twitter"
+          <input type="text" placeholder="@"
             onChange={(event) => props.onChange("twitter", event.target.value)}/>
         </div>
-
-          <button type="button" onClick={(event) => props.handleSubmit(event)}>Post</button>
+          <button className={postButton} type="button" onClick={(event) => props.handleSubmit(event)}>Go!</button>
       </form>
       </div>
   )
