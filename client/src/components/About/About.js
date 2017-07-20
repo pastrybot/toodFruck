9a7643bf9fde40b2dc286f75f51058ba84fd0827
asternaut <snooks.sheild@gmail.com>
2017-07-20 14:47:09 -0600

    about page and color styling

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import {about, peeps, solo, whatItIs, contact} from './aboutStyle.css';
 
 const About = (props) => {
   return (
-    <div>
-      <h1>About Content here</h1>
+    <div className={about}>
+      <h1>The Tood Fruck Crew</h1>
+      <div className={peeps}>
+
+        <div className={solo}>
+          <figure>
+            <img src="https://unsplash.it/200/200/?random" alt="Katie" />
+          </figure>
+          <h4> Katie </h4>
+          <figcaption> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dui in nibh mollis dignissim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </figcaption>
+        </div>
+
+        <div className={solo}>
+          <figure>
+            <img src="https://unsplash.it/200/200/?random" alt="Sienna" />
+          </figure>
+          <h4> Sienna </h4>
+          <figcaption> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dui in nibh mollis dignissim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </figcaption>
+        </div>
+
+        <div className={solo}>
+          <figure>
+            <img src="https://unsplash.it/200/200/?random" alt="Tyler" />
+          </figure>
+          <h4> Tyler </h4>
+          <figcaption> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dui in nibh mollis dignissim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </figcaption>
+        </div>
+      </div>
+
+      <div className={whatItIs}>
+        <h4> Here to make finding food a little less sucky </h4>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+      </div>
+
+      <div className={contact}>
+        <h4> Reach out to us:  </h4>
+        <label> Your Name </label>
+        <input type="text" />
+        <label> Email </label>
+        <input type="email" />
+        <label> Comments & Questions: </label>
+        <textarea />
+        <button type="button"> submit </button>
+      </div>
+
     </div>
   )
 }
--- client/src/components/MarkerWrapper/MarkerWrapper.js
+++ client/src/components/MarkerWrapper/MarkerWrapper.js
@@ -1,8 +1,4 @@
 import React, {PropTypes, Component} from 'react';
-//import shouldPureComponentUpdate from 'react-pure-render/function';
-import {markerStyles, closedMarkerStyles} from './MarkerStyles.css';
-import $ from 'jquery';
-
 
 class MarkerWrapper extends Component {
   static propTypes = {
@@ -15,8 +11,7 @@ class MarkerWrapper extends Component {
   render() {
     console.log(this.props, "PROPS");
     return (
-       <div onClick={() => this.props.toggleCard(this.props.id)}
-        className={this.props.isActive ? markerStyles : closedMarkerStyles}>
+       <div onClick={() => this.props.toggleCard(this.props.id)}>
           {this.props.children}
        </div>
     );
