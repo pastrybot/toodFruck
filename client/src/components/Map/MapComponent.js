b3e76516b12f857704856dc67ddc27c3523a34e0
asternaut <snooks.sheild@gmail.com>
2017-07-20 19:47:16 -0600

    map styles!

--- client/src/components/Map/MapComponent.js
+++ client/src/components/Map/MapComponent.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-// import Map from 'google-map-react'; ????
 import MarkerWrapper from '../MarkerWrapper/MarkerWrapper';
 import {MapStyle, truckPin} from './MapStyle.css';
 import TruckCard from '../TruckCard/TruckCard';
-
+import FancyStyle from './FancyStyle'
 
 const MapComponent = (props) => {
   const mapDimensions = {
       width: '80%',
       height: '100%',
     };
+
+    const mapOptions = {
+      styles: FancyStyle
+    };
+
   return(
     <div style={mapDimensions} className={MapStyle}>
     <GoogleMapReact
           onClick={() => props.clickMap()}
           defaultCenter={props.center}
           defaultZoom={props.zoom}
+          options={mapOptions}
           bootstrapURLKeys={{key:'AIzaSyCsXWyNVyTn1_WFbABZWA0NHk-qsNwx0gg'}}
     >
     { props.trucks.map((truck, index) =>
