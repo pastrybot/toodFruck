7ac0415e7be7e0c100529191ae7abc1e58e4e287
asternaut <snooks.sheild@gmail.com>
2017-07-20 18:52:23 -0600

    lil update

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -33,7 +33,7 @@ const About = (props) => {
       </div>
 
       <div className={whatItIs}>
-        <h4> Here to make finding food a little less sucky </h4>
+        <h4> Here to make finding nom noms a little less sucky </h4>
         <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
       </div>
 
